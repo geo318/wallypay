b6b684da5b2b63fedf86f5522f3ab7542a6805fd
geo318 <geo.lomidze@gmail.com>
2023-12-28 13:57:35 +0400

    feat: add rates page

--- app/[lang]/terms/page.tsx
+++ app/[lang]/terms/page.tsx
@@ -1,23 +1,26 @@
-import { getDictionary } from '/lib'
-import { PageProps } from '/types'
-import { DownloadApp } from '/components'
-import Link from 'next/link'
-import { terms as termItems } from '/config'
+import { getDictionary } from "/lib";
+import { PageProps } from "/types";
+import { DownloadApp } from "/components";
+import { terms as termItems } from "/config";
+import Link from "next/link";
 
 export default async function DebitCard({ params: { lang } }: PageProps) {
-  const { terms } = await getDictionary(lang)
+  const { terms } = await getDictionary(lang);
   return (
     <>
-      <article className='text-black text-lg fade-in xl:py-20 pt-10 flex flex-col'>
-        <h1 className='xl:text-4xl text-2xl font-bold xl:px-16 px-5'>
+      <article className="text-black text-lg fade-in xl:py-20 pt-10 flex flex-col">
+        <h1 className="xl:text-4xl text-2xl font-bold xl:px-16 px-5">
           {terms.h1}
         </h1>
-        <ul className='flex flex-col gap-5 xl:p-16 py-10 px-5 relative'>
-          {termItems.map(({ link, key }) => (
-            <li key={key} className='list-disc ml-5'>
+        <ul className="flex flex-col gap-5 xl:p-16 py-10 px-5 relative">
+          {termItems.map(({ link, download, key }) => (
+            <li key={key} className="list-disc ml-5">
               <Link
-                href={`/${lang}${link}`}
-                className='hover:text-app-blue hover:underline cursor-pointer'
+                href={`${
+                  download ? process.env.NEXT_PUBLIC_URL : `/${lang}`
+                }${link}`}
+                className="hover:text-app-blue hover:underline cursor-pointer"
+                download={download}
               >
                 {terms.li[key]}
               </Link>
@@ -27,5 +30,5 @@ export default async function DebitCard({ params: { lang } }: PageProps) {
       </article>
       <DownloadApp />
     </>
-  )
+  );
 }
--- config/pages/terms.ts
+++ config/pages/terms.ts
@@ -1,9 +1,10 @@
-import { routes } from '../routes'
+import { routes } from "../routes";
 
 export const terms = [
-  { key: 'policy', link: routes.policy },
-  { key: 'security', link: routes.security },
-  { key: 'rules', link: routes.rules },
-  { key: 'confidentiality', link: routes.confidentiality },
-  { key: 'agreement', link: routes.agreement },
-] as const
+  { key: "policy", link: routes.policy, download: false },
+  { key: "security", link: routes.security, download: false },
+  { key: "rules", link: routes.rules, download: false },
+  { key: "confidentiality", link: routes.confidentiality, download: false },
+  { key: "agreement", link: routes.agreement, download: false },
+  { key: "rates", link: routes.rates, download: true },
+] as const;
